e735cf1f9635a4b97555185699436e320073ef28
Mac <sjc123580@gmail.com>
2023-06-16 18:42:46 +0800

    pages code

--- DemoProject/src/main/resources/static/pages/createNewQuestionnaire/index.js
+++ DemoProject/src/main/resources/static/pages/createNewQuestionnaire/index.js
@@ -1,5 +1,5 @@
 onload = () => {
-  $('#headerUsername').text($util.getItem('userInfo').username)
+  $('#headerUsername').text($util.getItem('userInfo')[0].username)
   $('#headerDivB').text('创建调查问卷')
 
   $('#startTime').datetimepicker({
