fea793d8c7fda80412bf806a565fbd46e58e02f4
Mac <sjc123580@gmail.com>
2023-06-26 12:50:42 +0800

    QuestionnaireController and js

--- DemoProject/src/main/resources/static/pages/seeQuestionnaire/index.js
+++ DemoProject/src/main/resources/static/pages/seeQuestionnaire/index.js
@@ -1,34 +1,20 @@
 onload = () => {
     $('#headerUsername').text($util.getItem('userInfo')[0].username)
-    let projectId = $util.getPageParam('seeProject')
-    console.log(projectId, 'projectId')
-    fetchQuestionnaireList(projectId)
+    $('#headerDivB').text('答卷详情')
+
+    let questionnaieId = $util.getPageParam('questionnaireId')
+    console.log(questionnaieId, 'questionnaireId')
+    fetchQuestionnaireList(questionnaieId)
 }
 
 const fetchQuestionnaireList = (id) => {
     let params = {
-        // createdBy: $util.getItem('userInfo')[0].username
         id
     }
     $.ajax({
-        url: API_BASE_URL + '/queryProjectList',
+        url: API_BASE_URL + '/queryAnswerSheetInfo',
         type: "POST",
         data: JSON.stringify(params),
-        dataType: "json",
-        contentType: "application/json",
-        success(res) {
-            let info = res.data[0]
-            console.log(info, 'res')
-        }
-    })
-
-    let params2 = {
-        projectId: id
-    }
-    $.ajax({
-        url: API_BASE_URL + '/queryQuestionnaireInfo',
-        type: "POST",
-        data: JSON.stringify(params2),
         dataType: "json",
         contentType: "application/json",
         success(res) {
@@ -39,10 +25,10 @@ const fetchQuestionnaireList = (id) => {
             let tbody = $('.table').find("tbody");
             tbody.empty(); // 清空表格内容，避免重复插入
 
-            info.forEach((item, index) => {
+            info.forEach((item) => {
                 let tr = $("<tr></tr>");
-                let QNameTd = $("<td></td>").text(index + 1); // 问卷名称
-                let UNameTd = $("<td></td>").text(item.questionnaireName); // 答卷人
+                let QNameTd = $("<td></td>").text(item.questionnaireName); // 问卷名称
+                let UNameTd = $("<td></td>").text(item.answeruser); // 答卷人
                 let timeTd = $("<td></td>").text(item.creationDate); // 发布时间
 
                 let operationTd = $("<td></td>");
