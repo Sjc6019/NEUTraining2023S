0cf36ef10059bf4a5b13676d0ecdd7163cfbeff3
Mac <sjc123580@gmail.com>
2023-06-28 18:46:47 +0800

    search

--- DemoProject/src/main/resources/static/pages/seeQuestionnaire/index.js
+++ DemoProject/src/main/resources/static/pages/seeQuestionnaire/index.js
@@ -1,5 +1,6 @@
 let questionnaireName = '';
 let currentPage = 1;
+let answerUserName = '';
 
 onload = () => {
     $('#headerUsername').text($util.getItem('userInfo')[0].username);
@@ -14,9 +15,45 @@ onload = () => {
 };
 
 const onQSearch = () => {
-    let questionnaireId = $util.getPageParam('questionnaireId');
-    console.log(questionnaireId, 'questionnaireId');
-    fetchQuestionnaireList(questionnaireId, currentPage);
+    let answerUser = $('#answerUser').val();
+    console.log(answerUser, 'answerUser');
+
+    let params = {
+        answerUser: answerUser
+    };
+    $.ajax({
+        url: API_BASE_URL + '/queryAnswerSheetInfo',
+        type: "POST",
+        data: JSON.stringify(params),
+        dataType: "json",
+        contentType: "application/json",
+        success(res) {
+            console.log(res, 'res');
+            let info = res.data;
+
+            let tbody = $('.table').find("tbody");
+            tbody.empty(); // 清空表格内容，避免重复插入
+            info.forEach((item) => {
+                let tr = $("<tr></tr>");
+                let QNameTd = $("<td></td>").text(questionnaireName); // 问卷名称
+                let UNameTd = $("<td></td>").text(item.answerUser); // 答卷人
+                let timeTd = $("<td></td>").text(item.answerTime); 
+
+                let operationTd = $("<td></td>");
+                let operationBtn1 = $("<button></button>").text("明细").addClass("btn btn-primary");
+                operationBtn1.on("click", function () {
+                    console.log("操作按钮点击，当前行数据：", item);
+                    $util.setPageParam('answerUser', item.answerUser);
+                    console.log($util.getPageParam('answerUser'), 'answerUser');
+                    $util.setPageParam('answerId', item.id);
+                    console.log($util.getPageParam('answerId'), 'answerId');
+                    location.href = "/pages/showAnswerSheet/index.html";
+                });
+                tr.append(QNameTd, UNameTd, timeTd, operationTd.append(operationBtn1));
+                tbody.append(tr);
+            });
+        }
+    });
 };
 
 const previousPage = () => {
@@ -58,6 +95,8 @@ const fetchQuestionnaireList = (id, page) => {
 
                 let operationTd = $("<td></td>");
                 let operationBtn1 = $("<button></button>").text("明细").addClass("btn btn-primary");
+                $util.setPageParam('answerUser', item.answerUser);
+                console.log($util.getPageParam('answerUser'), 'answerUser');
                 operationBtn1.on("click", function () {
                     console.log("操作按钮点击，当前行数据：", item);
                     $util.setPageParam('answerUser', item.answerUser);
@@ -89,6 +128,23 @@ const getQuestionnaireName = (id) => {
         success(res) {
             console.log("res", res);
             questionnaireName = res.data[0].questionnaireName;
+        }
+    });
+};
+
+const getAnswerUserName = (id) => {
+    let params = {
+        id: id
+    };
+    $.ajax({
+        url: API_BASE_URL + '/queryAnswerSheetInfo',
+        type: "POST",
+        data: JSON.stringify(params),
+        dataType: "json",
+        contentType: "application/json",
+        success(res) {
+            console.log("res", res);
+            answerUserName = res.data[0].answerUser;
         }
     });
 };
